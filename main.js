5a157b5ae8077781b58071b67f7b199bfd15213d
Rui Huang <rcetger1@gmail.com>
2025-06-18 19:11:40 +0200

    Input Nacheinnander Integriert

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-
 /*Main*/
 
 const fullData = [];
-fetchData();
+const Area = [];
+const Category = [];
 
-/*Komplette API Datenbank zusammengefügt*/
-async function fetchData() {
+main()
+
+async function main(){
+    await fetchDataArea();
+    await fetchDataAll();
+    AreaInputSelection();
+
+}
+
+
+/*API Fetch für AreaDaten*/
+
+async function fetchDataArea() {
 
     try {
-        Area = [];
-        const datalist = document.getElementById('Area');
         const AreaData = await fetch(`https://www.themealdb.com/api/json/v1/1/list.php?a=list`);
 
         if (!AreaData.ok) {
@@ -19,77 +28,98 @@ async function fetchData() {
 
         if (Array.isArray(data.meals)) {
             Area.push(...data.meals);
-            console.log(Area);
 
-            for (let i = 0; i < 29;i++){
-                const option = document.createElement("option");
-                option.text = Area[i].strArea;
-                datalist.appendChild(option);
-            }
         }
     }
-
-    catch(error) {
-        console.error(error);
+    catch(error){
+            console.error(error);
     }
-
-    try {
-        Area = [];
-        const datalist = document.getElementById('Category');
-        const AreaData = await fetch(`https://www.themealdb.com/api/json/v1/1/list.php?c=list`);
-
-        if (!AreaData.ok) {
-            throw new Error("Could not fetch resource");
-        }
-        const data = await AreaData.json();
-
-        if (Array.isArray(data.meals)) {
-            Area.push(...data.meals);
-
-            for (let i = 0; i < 14;i++){
-                const option = document.createElement("option");
-                option.text = Area[i].strCategory;
-                datalist.appendChild(option);
-            }
-        }
-    }
-
-    catch(error) {
-        console.error(error);
-
-    }
-
-    try {
-        for (let i = 0; i < 26; i++) {
-
-            const letter = String.fromCharCode(97 + i);
-            const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`);
-
-            if (!response.ok) {
-                throw new Error("Could not fetch resource");
-            }
-            const data = await response.json();
-
-            if (Array.isArray(data.meals)) {
-                fullData.push(...data.meals);
-            }
-        }
-    }
-
-    catch(error) {
-        console.error(error);
-    }
-
-    return fullData;
 }
 
 
-/*Handling der Ingredient,Are Input*/
-function mainDivShiftTop(){
+/*API Fetch für FullDaten*/
+
+async function fetchDataAll() {
+    try {
+        const promises = [];
+        for (let i = 0; i < 26; i++) {
+            const letter = String.fromCharCode(97 + i);
+            promises.push(fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`));
+        }
+
+        const responses = await Promise.all(promises);
+        const data = await Promise.all(responses.map(r => r.json()));
+
+        fullData.push(...data.flatMap(d => d.meals || []));
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+/*Generate Input selection of Area*/
+
+function AreaInputSelection(){
+
+    const datalist = document.getElementById('Area');
+
+    for (let i = 0; i < 29; i++) {
+        const option = document.createElement("option");
+        option.text = Area[i].strArea;
+        datalist.appendChild(option);
+    }
+    console.log('test')
+}
+
+/*Switch von Area zu Category*/
+
+function AreaInput() {
+
+    document.querySelector('.Area-Button-active').classList.add("Area-Button-inactive");
+    document.querySelector('.Area-Input-active').classList.add("Area-Input-inactive");
+    document.querySelector('.Category-Button-inactive').classList.add("Category-Button-active");
+    document.querySelector('.Category-Input-inactive').classList.add("Category-Input-active");
+
+    const inputArea = document.querySelector('.Area-Input-active').value;
+
+    // Filter Category by AreaInput
+    const datalist = document.getElementById('Category');
+    datalist.innerHTML = '';
+
+    const uniqueCategories = new Set();
+    fullData.filter(meal => meal.strArea === inputArea)
+        .forEach(meal => uniqueCategories.add(meal.strCategory));
+
+    uniqueCategories.forEach(category => {
+        const option = document.createElement("option");
+        option.value = category;
+        datalist.appendChild(option);
+    });
+}
+
+/*Switch von Category zu Area + Generierung Rezept,Foto,Bild,Name*/
+function CategoryInput(){
+
+    /*Switch von Category zu Area*/
+    const AreaButton = document.getElementsByClassName("Area-Button-active")[0];
+    const AreaInput = document.getElementsByClassName("Area-Input-active")[0];
+
+    const CategoryButton = document.getElementsByClassName("Category-Button-inactive")[0];
+    const CategoryInput = document.getElementsByClassName("Category-Input-inactive")[0];
+
+    AreaButton.classList.remove("Area-Button-inactive");
+    AreaInput.classList.remove("Area-Input-inactive");
+
+    CategoryButton.classList.remove("Category-Button-active");
+    CategoryInput.classList.remove("Category-Input-active");
+
+    //Shift to the Top and display Flipcards
     const MainDiv = document.getElementsByClassName("main-div")[0];
     const FoodItem = document.getElementsByClassName("Food-Item")[0];
-    const Area = document.getElementsByClassName("Area-Input")[0].value;
-    const Category = document.getElementsByClassName("Category-Input")[0].value;
+
+    FoodItem.classList.add("Food-Item-Active");
+    MainDiv.classList.add("shifted");
+
+    //Add Picture Recipe Name to Flipcards
 
     const p0 = document.getElementsByClassName("Food-Item-p0")[0];
     const p1 = document.getElementsByClassName("Food-Item-p1")[0];
@@ -99,18 +129,17 @@ function mainDivShiftTop(){
     const recipeText0 = document.getElementsByClassName("Food-Item-p0-back")[0];
     const recipeText1 = document.getElementsByClassName("Food-Item-p1-back")[0];
 
-    const fullDataFiltered = fullData.filter(Data => (Data.strArea == Area && Data.strCategory == Category
-        )
+    const areaValue = document.querySelector('input[list="Area"]').value;
+    const categoryValue = document.querySelector('input[list="Category"]').value;
+
+    const fullDataFiltered = fullData.filter(Data =>
+        Data.strArea === areaValue &&
+        Data.strCategory === categoryValue
     );
-
-    FoodItem.classList.add("Food-Item-Active");
-    MainDiv.classList.add("shifted");
-
-    console.log(fullDataFiltered.length);
 
     if (fullDataFiltered.length == 1) {
 
-        p1.innerHTML = "0 recipes try different combination";
+        p1.innerHTML = "There is only 1 Recipe for this Combination";
         img1.src = "";
         recipeText1.innerHTML = "";
 
@@ -138,23 +167,6 @@ function mainDivShiftTop(){
             ul0.appendChild(li);
         }
     }
-
-    else if (fullDataFiltered.length == 0) {
-        img0.src = null;
-        p0.innerHTML = "0 recipes try different combination";
-        recipeText0.innerHTML = "";
-        const ul0 = document.getElementsByClassName("Unordered-List-0")[0];
-
-        ul0.innerHTML = "";
-        img1.src = null;
-        p1.innerHTML = "0 recipes try different combination";
-        recipeText1.innerHTML = "";
-
-        const ul1 = document.getElementsByClassName("Unordered-List-1")[0];
-        ul1.innerHTML = "";
-    }
-
-
 
     else{
         /*Generate Random Number */
@@ -211,6 +223,12 @@ function mainDivShiftTop(){
 
     }
 }
+
+
+
+
+
+
 
 
 
